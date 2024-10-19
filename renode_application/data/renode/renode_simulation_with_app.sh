59b70c68cbee325df5abc827d5a5cf812a118e38
root <dharanikanna1999@gamil.com>
2024-10-19 20:04:23 +0000

    Updating commands for Docker conatainer

--- renode_application/data/renode/renode_simulation_with_app.sh
+++ renode_application/data/renode/renode_simulation_with_app.sh
@@ -1,9 +1,11 @@
+# After turning in renode server try below commands for board specific testing and simuation
+
 # to create machine architecture first time
 mach create
 machine LoadPlatformDescription @platforms/boards/stm32f4_discovery-kit.repl
 # you have to copy location of .\build\src\STM32F4Template.elf
 # sysbus LoadELF -> load the elf
-sysbus LoadELF "/home/uif02672/qemu/emulator_setup/cortex-m7-emulator/renode_application/data/renode/build/src/STM32F4Template.elf"
+sysbus LoadELF "/usr/src/embedded_emulator/data/emulator/cortex-m7-emulator/renode_application/data/renode/build/src/STM32F4Template.elf"
 # open a terminal to display UART data
 showAnalyzer sysbus.usart2
 # run our simulation
--- run_debugger.sh
+++ run_debugger.sh
@@ -1,8 +1,9 @@
 #setting debugger path
-export PATH=$PATH:/home/uif02672/qemu/emulator_setup/cortex-m7-emulator/arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi/bin
+export PATH=$PATH:/usr/src/embedded_emulator/data/emulator/cortex-m7-emulator/arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi/bin
 
 #loading application on debugger
-arm-none-eabi-gdb app.elf
+# arm-none-eabi-gdb app.elf
+arm-none-eabi-gdb /usr/src/embedded_emulator/data/emulator/cortex-m7-emulator/renode_application/data/renode/build/src/STM32F4Template.elf
 
 #after gdb enables run the below command to run target port
 target remote localhost:1234
